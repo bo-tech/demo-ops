813645cc2b30c340338a316bf65e3dcb220dc03c
Johannes Bornhold <johannes@bornhold.name>
2026-09-02 14:43:18 +0200

    refactor: Name the systems the flake builds for

--- flake.nix
+++ flake.nix
@@ -115,7 +115,10 @@
       };
     };
   }
-  // flake-utils.lib.eachDefaultSystem (system: let
+  // flake-utils.lib.eachSystem [
+    "x86_64-linux"
+    "aarch64-linux"
+  ] (system: let
     pkgs = nixpkgs.legacyPackages.${system};
   in {
     devShells.default = pkgs.mkShell {
